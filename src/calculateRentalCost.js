df83f854005bee584234041b5f1ceccc042eb975
Mechanic <v.prushak@gmail.com>
2025-11-06 09:41:58 +0200

    solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -3,8 +3,14 @@
  *
  * @return {number}
  */
+
+const LONG_TERM_DISC_7 = 50;
+const LONG_TERM_DISC_3 = 20;
+const DAILY_PRICE = 40;
+
 function calculateRentalCost(days) {
-  // write code here
+  return days >= 7 ? (days * DAILY_PRICE) - LONG_TERM_DISC_7 :
+            days >= 3 ? (days * DAILY_PRICE) - LONG_TERM_DISC_3 : days * DAILY_PRICE;
 }
 
 module.exports = calculateRentalCost;
